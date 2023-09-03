58007899faa73778ffe6711995bef56908daff71
luchostudy <luchostudy@MacBook-Pro-de-Lucho.local>
2023-09-03 01:35:19 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
-  },
   server: {
     host: '0.0.0.0',
     port: 3001,
